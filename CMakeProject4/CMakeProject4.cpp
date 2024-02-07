46b71abd7f218174e834a9abdaedefd712240291
nikdiz <nikdizprog@mail.ru>
2024-02-07 19:56:41 +0100

    initial commit

---
+++ CMakeProject4/CMakeProject4.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject4.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject4.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
